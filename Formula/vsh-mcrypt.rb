acf895438cc64e2cf2640ae4968b0f3781e91a80
Philipp Dittert <p.dittert@techdivision.com>
2026-01-21 15:19:56 +0100

    add mcrypt Formula

---
+++ Formula/vsh-mcrypt.rb
@@ -0,0 +1,82 @@
+class VshMcrypt < Formula
+  desc "Replacement for the old crypt package and crypt(1) command"
+  homepage "https://mcrypt.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/mcrypt/MCrypt/2.6.8/mcrypt-2.6.8.tar.gz"
+  sha256 "5145aa844e54cca89ddab6fb7dd9e5952811d8d787c4f4bf27eb261e6c182098"
+  # revision 1
+
+  bottle do
+    root_url "https://ghcr.io/v2/valet-sh/php"
+    sha256 cellar: :any, arm64_tahoe: "b6018711f6ca639efda97a81995a0126d3ec6bb5e7082fc544bdc07610408bae"
+  end
+
+  # Added automake as a build dependency to update config files in libmcrypt.
+  # Please remove in future if there is a patch upstream which recognises aarch64 macos.
+  depends_on "automake" => :build
+  depends_on "libtool"
+  depends_on "mhash"
+
+  uses_from_macos "zlib"
+
+  resource "libmcrypt" do
+    url "https://downloads.sourceforge.net/project/mcrypt/Libmcrypt/2.5.8/libmcrypt-2.5.8.tar.gz"
+    sha256 "e4eb6c074bbab168ac47b947c195ff8cef9d51a211cdd18ca9c9ef34d27a373e"
+  end
+
+  # Patch to correct inclusion of malloc function on OSX.
+  # Upstream: https://sourceforge.net/p/mcrypt/patches/14/
+  patch :DATA
+
+  def install
+    # Work around configure issues with Xcode 12
+    ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
+    ENV.append "CFLAGS", "-Wno-implicit-int"
+
+    resource("libmcrypt").stage do
+      # Workaround for ancient config files not recognising aarch64 macos.
+      %w[config.guess config.sub].each do |fn|
+        cp "#{Formula["automake"].opt_prefix}/share/automake-#{Formula["automake"].version.major_minor}/#{fn}", fn
+      end
+
+      # Avoid flat_namespace usage on macOS
+      inreplace "./configure", "${wl}-flat_namespace ${wl}-undefined ${wl}suppress", "" if OS.mac?
+
+      system "./configure", "--prefix=#{prefix}",
+                            "--mandir=#{man}"
+      system "make", "install"
+    end
+
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-libmcrypt-prefix=#{prefix}",
+                          "--mandir=#{man}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.txt").write <<~EOS
+      Hello, world!
+    EOS
+    system bin/"mcrypt", "--key", "TestPassword", "--force", "test.txt"
+    rm "test.txt"
+    system bin/"mcrypt", "--key", "TestPassword", "--decrypt", "test.txt.nc"
+  end
+end
+
+__END__
+diff --git a/src/rfc2440.c b/src/rfc2440.c
+index 5a1f296..aeb501c 100644
+--- a/src/rfc2440.c
++++ b/src/rfc2440.c
+@@ -23,7 +23,12 @@
+ #include <zlib.h>
+ #endif
+ #include <stdio.h>
++
++#ifdef __APPLE__
++#include <malloc/malloc.h>
++#else
+ #include <malloc.h>
++#endif
+
+ #include "xmalloc.h"
+ #include "keys.h"
