310c627ce3cfe9f80fc4d9758a35e0d68188c51c
Philipp Dittert <p.dittert@techdivision.com>
2026-06-26 13:20:18 +0200

    Update vsh-php73.rb

--- Formula/vsh-php73.rb
+++ Formula/vsh-php73.rb
@@ -73,6 +73,8 @@ class VshPhp73 < Formula
   patch :DATA
 
   def install
+    ENV.append "CFLAGS", "-std=gnu17"
+    
     # Work around configure issues with Xcode 15
     ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
 
