b88fd42b5e1f54f1327c69a48e2518e1ca2f53db
Philipp Dittert <p.dittert@techdivision.com>
2026-06-26 14:28:31 +0200

    Update vsh-php80.rb
    Closes #40.
    
    Signed-off-by: Philipp Dittert <philipp.dittert@gmail.com>

--- Formula/vsh-php80.rb
+++ Formula/vsh-php80.rb
@@ -75,7 +75,7 @@ class VshPhp80 < Formula
 
   def install
     ENV.append "CFLAGS", "-std=gnu17"
-    
+
     # Work around for building with Xcode 15.3
     if DevelopmentTools.clang_build_version >= 1500
       ENV.append "CFLAGS", "-Wno-incompatible-function-pointer-types"
